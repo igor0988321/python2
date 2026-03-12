e54cd3b25d3b4033c71b86bc17efdb2699ac22f4
Ruslan <karnaukhruslan6@gmail.com>
2026-03-12 14:15:05 +0200

    Added a command menu, added the ability to show time

--- Telegram.bot.py
+++ Telegram.bot.py
@@ -1,8 +1,9 @@
 import asyncio
+from datetime import datetime
 
 from aiogram import F, Bot, Dispatcher
 from aiogram.types import Message
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 
 bot = Bot(token="")
 dp = Dispatcher()
@@ -13,9 +14,26 @@ dp = Dispatcher()
 async def on_start(message: Message):
     await message.answer(f"Hello {message.from_user.username}")
 
+
+@dp.message(Command("help"))
+async def help(message: Message):
+    await message.answer(
+        "Commands:\n"
+        "start - start the bot\n"
+        "help - help\n"
+        "time - current time\n"
+    )
+
 @dp.message(F.text == "Hi")
 async def on_hi(message: Message):
     await message.reply(f"Hello {message.from_user.username}, how are you?")
+
+@dp.message(Command('time'))
+async def time(message: Message):
+    now = datetime.now().strftime("%H:%M")
+    await message.answer(f"Now is the time: {now}")
+
+
 
 @dp.message()
 async def on_message(message: Message):
