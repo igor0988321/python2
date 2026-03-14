5382ea103041f5f2029c1262446c02ba9b7371d1
Ruslan <karnaukhruslan6@gmail.com>
2026-03-14 14:28:55 +0200

    Debugging, added a graphical button for the profile

--- Telegram.bot.py
+++ Telegram.bot.py
@@ -2,37 +2,60 @@ import asyncio
 from datetime import datetime
 
 from aiogram import F, Bot, Dispatcher
-from aiogram.types import Message
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.filters import CommandStart, Command
 
-bot = Bot(token="")
+bot = Bot(token="8210545761:AAFWTOG5tRpbWDtgi9Axlrxl5rOC6alUujs")
 dp = Dispatcher()
 
 
+menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Profile")]
+    ],
+    resize_keyboard=True
+)
 
 @dp.message(CommandStart())
 async def on_start(message: Message):
     await message.answer(f"Hello {message.from_user.username}")
+    await message.answer("Main menu", reply_markup=menu)
 
 
 @dp.message(Command("help"))
-async def help(message: Message):
+async def help_cmd(message: Message):
     await message.answer(
         "Commands:\n"
-        "start - start the bot\n"
-        "help - help\n"
-        "time - current time\n"
+        "/start - start the bot\n"
+        "/help - help\n"
+        "/time - current time\n"
     )
 
-@dp.message(F.text == "Hi")
-async def on_hi(message: Message):
-    await message.reply(f"Hello {message.from_user.username}, how are you?")
 
 @dp.message(Command('time'))
 async def time(message: Message):
     now = datetime.now().strftime("%H:%M")
     await message.answer(f"Now is the time: {now}")
 
+@dp.message(F.text == "Profile")
+async def profile(message: Message):
+
+    user_id = message.from_user.id
+    user_name = message.from_user.username
+    name = message.from_user.first_name
+
+    text = f"""
+    Yoy profile
+    ID: {user_id}
+    Name: {name}
+    Username: {user_name}
+    """
+
+    await message.answer(text)
+
+@dp.message(F.text == "Hi")
+async def on_hi(message: Message):
+    await message.reply(f"Hello {message.from_user.username}, how are you?")
 
 
 @dp.message()
@@ -40,6 +63,8 @@ async def on_message(message: Message):
     await message.answer(
         message.text.upper(),
     )
+
+
 
 async def main():
     await dp.start_polling(bot)
