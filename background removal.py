840957d3f6b838b013ba13aa2dc03689714918df
Ruslan <karnaukhruslan6@gmail.com>
2026-03-31 18:46:16 +0300

    31.03.2026

---
+++ background removal.py
@@ -0,0 +1,14 @@
+from renbg import remove
+from PIL import Image
+
+def remove_background(input_path, output_path):
+    input_image = Image.open(input_path)
+    output_image = remove(input_image)
+    output_image.save(output_path)
+
+
+if __name__ == '__main__':
+    input_path = 'input_image.png'
+    output_path = 'output_image.png'
+    remove_background(input_path, output_path)
+    print(f"Output image saved at {output_path}")
