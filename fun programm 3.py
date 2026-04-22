13c1fc07cb2072c026e2164f5892a1ed4a17f0cd
Ruslan <karnaukhruslan6@gmail.com>
2026-04-22 14:52:59 +0300

    22.04.2026

--- fun programm 3.py
+++ fun programm 3.py
@@ -1,16 +1,4 @@
-strings = [
-    "banana", "python",
-    "lviv", "apple",
-    "Paris"
-]
-
-# print(strings[1])
-
-count = 0
-
-for index in range(5):
-    string = len(strings[index - 1])
-    if index != 0:
-        count += string
-
-    print(" " * count + strings[index])
+text = input("Enter text: ")
+text += ' '
+for i in range(1,6):
+    print(text*i)
